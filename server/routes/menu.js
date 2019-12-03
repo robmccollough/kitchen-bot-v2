a0b6bec0b104c8011311678228a422f21b07d9e3
robmccollough <admin@robmccollough.com>
2019-12-03 15:18:21 -0500

    moved client

--- server/routes/menu.js
+++ server/routes/menu.js
@@ -7,16 +7,7 @@ const authAdmin = require('../middlewares/authAdmin.js')
 //possibly move db logic out of routing
 
 
-router.post('/', authAdmin, (req, res) => {
-    (new Menu({
-        food: req.body.food
-    })).save().then( (result) => {
-        res.send(result)
-    }).catch( (err) => {
-        res.send(err)
-    })
-       
-})
+
 
 router.get('/', auth, (req, res) => {
     Menu.findOne({}, {}, { sort: { 'date' : -1 } }, (err, doc) => {
@@ -47,6 +38,22 @@ router.get('/all', authAdmin, (req, res) => {
 
 router.delete('/', authAdmin, (req, res) => {
     Menu.deleteOne({ _id : req.body.menu_id}).then( result => res.send(result)).catch( err => res.send(err))
+})
+
+router.post('/', authAdmin, (req, res) => {
+    (new Menu({
+        food: req.body.food
+    })).save().then( (result) => {
+        res.send(result)
+    }).catch( (err) => {
+        res.send(err)
+    })  
+})
+
+router.put('/', authAdmin, (req, res) => {
+    Menu.updateOne({_id : req.body.menu_id}, req.body).then( (result) => {
+        res.send(result)
+    })
 })
 
 module.exports = {router: router, prefix: '/menu'}
