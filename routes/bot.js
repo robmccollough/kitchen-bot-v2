687937f41beae8dc3a354e0811cb79648d076340
Rob McCollough <admin@robmccollough.com>
2020-01-13 18:10:26 -0500

    bot id fix

--- routes/bot.js
+++ routes/bot.js
@@ -6,27 +6,27 @@ const botService = require("../middlewares/bot");
 
 //only single endpoint
 router.post(
-	"/",
-	botService.parseBotRequest,
-	botService.fetchBotResponse,
-	(req, res) => {
-		//response generated through middleware
-		//post request to groupme
-		axios({
-			method: "POST",
-			url: process.env.BOT_POST_URL,
-			data: {
-				bot_id: process.env.TEST_BOT_ID,
-				text: req.bot_response || "Hi There"
-			}
-		})
-			.then(() => {
-				res.send({ command: req.command, response: req.bot_response });
-			})
-			.catch(err => console.log(err));
+  "/",
+  botService.parseBotRequest,
+  botService.fetchBotResponse,
+  (req, res) => {
+    //response generated through middleware
+    //post request to groupme
+    axios({
+      method: "POST",
+      url: process.env.BOT_POST_URL,
+      data: {
+        bot_id: process.env.BOT_ID,
+        text: req.bot_response || "Hi There"
+      }
+    })
+      .then(() => {
+        res.send({ command: req.command, response: req.bot_response });
+      })
+      .catch(err => console.log(err));
 
-		//no response required
-	}
+    //no response required
+  }
 );
 
 module.exports = { router: router, prefix: "/bot" };
